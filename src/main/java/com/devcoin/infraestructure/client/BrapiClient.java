2b6c861f8827e26ba6f308d4d0ab228e359b859c
Araujo <cauaraujo@deloitte.com>
2025-08-26 13:59:06 -0300

    feat: implementacao de fallback do brapi client

--- src/main/java/com/devcoin/infraestructure/client/BrapiClient.java
+++ src/main/java/com/devcoin/infraestructure/client/BrapiClient.java
@@ -2,6 +2,7 @@ package com.devcoin.infraestructure.client;
 
 import com.devcoin.application.dto.BrapiResponse;
 import com.devcoin.infraestructure.client.config.BrapiClientConfig;
+import com.devcoin.infraestructure.client.fallback.BrapiClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(
         name = "brapi",
         url = "https://brapi.dev/api",
-        configuration = BrapiClientConfig.class
+        configuration = BrapiClientConfig.class,
+        fallback = BrapiClientFallback.class
 )
 public interface BrapiClient {
 
