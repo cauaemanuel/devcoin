83a4e107563bfa79ffeb4a6b6c84bca2efe2fbe0
Araujo <cauaraujo@deloitte.com>
2025-08-26 10:53:31 -0300

    start: project

---
+++ src/main/java/com/devcoin/DevcoinApplication.java
@@ -0,0 +1,13 @@
+package com.devcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devcoin/DevcoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
